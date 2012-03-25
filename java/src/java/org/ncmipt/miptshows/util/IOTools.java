e669504aabcfce9c0ae0b00ce19176337fdf25da
spitty.pnz@gmail.com <spitty.pnz@gmail.com@736d22bf-757c-8cdd-c422-fb1e8f1f9c69>
2012-03-25 14:58:05 +0000

    Max: Add package org.ncmipt.miptshows.util. Put classes IOTools(new) and DBUtils in it.

---
+++ java/src/java/org/ncmipt/miptshows/util/IOTools.java
@@ -0,0 +1,67 @@
+package org.ncmipt.miptshows.util;
+
+import java.io.Closeable;
+import java.lang.reflect.Method;
+import org.apache.log4j.LogManager;
+
+/**
+ * Util class simplifying IO operations<br/>
+ * <font color='red'>WARNING: untested yet</font>
+ * @author Spitty
+ */
+public class IOTools
+{
+
+    private static final org.apache.log4j.Logger LOG = LogManager.getLogger(IOTools.class);
+
+    /**
+     * Private constructor. It prevents one from creating instance of {@link IOTools}
+     */
+    private IOTools()
+    {
+    }
+
+    /**
+     * This method tries to close
+     * <code>resource</code> when it is a {@link Closeable} object
+     *
+     * @param resource {@link Closeable} object to be closed
+     */
+    public static void close(final Closeable resource)
+    {
+        try
+        {
+            if (resource != null)
+            {
+                resource.close();
+            }
+        }
+        catch (Exception ex)
+        {
+            LOG.error("Can't close the resource", ex);
+        }
+    }
+
+    /**
+     * This method tries to close
+     * <code>resource</code> by invoking method <code>close</code>
+     *
+     * @param resource {@link Closeable} object to be closed
+     */
+    public static void close(final Object resource)
+    {
+        try
+        {
+            if (resource == null)
+            {
+                return;
+            }
+            final Method closeMethod = resource.getClass().getDeclaredMethod("close", new Class[]{});
+            closeMethod.invoke(resource, new Object[]{});
+        }
+        catch (Exception ex)
+        {
+            LOG.error("Can't close the resource", ex);
+        }
+    }
+}
